12be1db21492a779ffc2ff362a1452ac7f2746d3
shiph <1278998669@qq.com>
2018-05-05 10:17:18 +0800

    客服

--- src/component/ToolsButton.js
+++ src/component/ToolsButton.js
@@ -7,7 +7,7 @@ import {
     StyleSheet
 } from 'react-native'
 
-export default class ToolsButtom extends Component {
+export default class ToolsButton extends Component {
 
     constructor() {
         super()
